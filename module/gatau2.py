4da9d84444070665ad9efb06bd2ae2e515220c57
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 21:15:37 +0700

    Update gatau2.py

--- module/gatau2.py
+++ module/gatau2.py
@@ -8,7 +8,7 @@ def mengetik(s):
         sys.stdout.write(c)
         sys.stdout.flush()
 #kecepatan mengetik
-        time.sleep(random.random() * 0.5)
+        time.sleep(random.random() * 0.1)
 #ubah angka 0.1 sesuai keinginan kamu
 #angka terkecil adalah yang paling cepat
 #angka terbesar adalah yang paling lambat
@@ -16,11 +16,18 @@ def mengetik(s):
 mengetik('Tar download file dulu.')
 mengetik('sabar ya anjing.')
 mengetik('installing file...')
+pkg install git
 mengetik('installing sugiono...')
+pkg install python
 mengetik('installing asiaporn.com.')
+pkg install wget
 mengetik('installing bokep jepang...')
+pkg install lolcat
 mengetik('installing bangbross.')
+pkg install cowsay
 mengetik('installing simontok...')
+pkg install sl
 mengetik('installing xnxx.com.')
+pkg update
 mengetik('selesai')
-time:25-3-2022
+sl
