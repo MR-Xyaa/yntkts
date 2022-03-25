76079c50a5ff1059d00c7f2b2f2e225bc53d2197
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 20:39:40 +0700

    Create gatau5.py

---
+++ module/gatau5.py
@@ -0,0 +1,14 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.5)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
+
+mengetik('MAAP NIH GABUT MAAP YA')
