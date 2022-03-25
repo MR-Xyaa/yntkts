b0022f55b1af38cc3357d8956939d351a4508e4e
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 21:36:22 +0700

    Update gatau2.py

--- module/gatau2.py
+++ module/gatau2.py
@@ -14,106 +14,41 @@ def mengetik(s):
 mengetik('Tar download file dulu.')
 mengetik('sabar ya anjing.')
 mengetik('installing file...')
-#!/system/xbin/bash/python
-pkg install git
-import random
-import sys
-import time
-def mengetik(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-#kecepatan mengetik
-        time.sleep(random.random() * 0.1)
-#ubah angka 0.1 sesuai keinginan kamu
-#angka terkecil adalah yang paling cepat
-#angka terbesar adalah yang paling lambat
-mengetik('installing sugiono...')
-#!/system/xbin/bash/python
-pkg install python
-import random
-import sys
-import time
-def mengetik(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-#kecepatan mengetik
-        time.sleep(random.random() * 0.1)
-#ubah angka 0.1 sesuai keinginan kamu
-#angka terkecil adalah yang paling cepat
-#angka terbesar adalah yang paling lambat
-mengetik('installing asiaporn.com.')
-#!/system/xbin/bash/python
-pkg install wgetimport random
-import sys
-import time
-def mengetik(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-#kecepatan mengetik
-        time.sleep(random.random() * 0.1)
-#ubah angka 0.1 sesuai keinginan kamu
-#angka terkecil adalah yang paling cepat
-#angka terbesar adalah yang paling lambat
-mengetik('installing bokep jepang...')
-#!/system/xbin/bash/python
-pkg install lolcat
-import random
-import sys
-import time
-def mengetik(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-#kecepatan mengetik
-        time.sleep(random.random() * 0.1)
-#ubah angka 0.1 sesuai keinginan kamu
-#angka terkecil adalah yang paling cepat
-#angka terbesar adalah yang paling lambat
-mengetik('installing bangbross.')
-#!/system/xbin/bash/python
-pkg install cowsayimport random
-import sys
-import time
-def mengetik(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-#kecepatan mengetik
-        time.sleep(random.random() * 0.1)
-#ubah angka 0.1 sesuai keinginan kamu
-#angka terkecil adalah yang paling cepat
-#angka terbesar adalah yang paling lambat
-mengetik('installing simontok...')
-#!/system/xbin/bash/python
-pkg install sl
-import random
-import sys
-import time
-def mengetik(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-#kecepatan mengetik
-        time.sleep(random.random() * 0.1)
-#ubah angka 0.1 sesuai keinginan kamu
-#angka terkecil adalah yang paling cepat
-#angka terbesar adalah yang paling lambat
-mengetik('installing xnxx.com.')
-pkg update
-import random
-import sys
-import time
-def mengetik(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-#kecepatan mengetik
-        time.sleep(random.random() * 0.1)
-#ubah angka 0.1 sesuai keinginan kamu
-#angka terkecil adalah yang paling cepat
-#angka terbesar adalah yang paling lambat
-mengetik('selesai')
-sl
+
+
+
+mengetik('1.installing sugiono')
+mengetik('kontol.....')
+mengetik('memek.....')
+mengetik('pecuh......')
+mengetik('crot......')
+mengetik('▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪')
+mengetik('▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪')
+mengetik('▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪')
+mengetik('▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪')
+mengetik('▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪')
+mengetik('▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪')
+mengetik('terinstall')
+mengetik('!')
+mengetik('!!!!!!!!!')
+mengetik('!!!!!!')
+mengetik('!!!!!!!!!')
+mengetik('!!!!!!!!!!!!!!!')
+mengetik('!!!!!!!!')
+mengetik('!!!!!!!!!!!!!!!!!!!!!')
+mengetik('2.installing xnxx.com')
+mengetik('turuk......')
+mengetik('jembut.......')
+mengetik('ngiclik.........')
+mengetik('bangbross......')
+mengetik('ngewe...........')
+mengetik('▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪')
+mengetik('▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪')
+mengetik('▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪')
+mengetik('■')
+mengetik('■')
+mengetik('■')
+mengetik('■')
+mengetik('■')
+mengetik('■')
+mengetik('■')
