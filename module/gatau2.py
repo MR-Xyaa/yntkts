f297324735380aa06bac5f398fb8a8bc486e6145
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 21:21:47 +0700

    Update gatau2.py

--- module/gatau2.py
+++ module/gatau2.py
@@ -1,5 +1,3 @@
-#!/system/xbin/bash/python
-
 import random
 import sys
 import time
@@ -16,18 +14,106 @@ def mengetik(s):
 mengetik('Tar download file dulu.')
 mengetik('sabar ya anjing.')
 mengetik('installing file...')
+#!/system/xbin/bash/python
 pkg install git
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
 mengetik('installing sugiono...')
+#!/system/xbin/bash/python
 pkg install python
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
 mengetik('installing asiaporn.com.')
-pkg install wget
+#!/system/xbin/bash/python
+pkg install wgetimport random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
 mengetik('installing bokep jepang...')
+#!/system/xbin/bash/python
 pkg install lolcat
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
 mengetik('installing bangbross.')
-pkg install cowsay
+#!/system/xbin/bash/python
+pkg install cowsayimport random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
 mengetik('installing simontok...')
+#!/system/xbin/bash/python
 pkg install sl
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
 mengetik('installing xnxx.com.')
 pkg update
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
 mengetik('selesai')
 sl
